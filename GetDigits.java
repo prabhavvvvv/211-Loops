b6eddf0b3613a3cb2c944576c9f7215a3607a4d0
prabhaaaav <108563497+prabhavvvvv@users.noreply.github.com>
2025-11-18 11:29:36 -0800

    Create GetDigits.java to input a positive integer

---
+++ GetDigits.java
@@ -0,0 +1,17 @@
+/*
+ * Activity 2.1.1
+ */
+import java.util.Scanner;
+
+public class GetDigits
+{
+  public static void main(String[] args)
+  {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter a positive integer to see its digits: ");
+    int number = sc.nextInt();
+
+    
+    
+  }
+}
